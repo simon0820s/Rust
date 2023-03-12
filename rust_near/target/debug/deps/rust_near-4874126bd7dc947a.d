df40f8a861ffa2678f61eea153cdaff511436d32
simon0820s <simon0820s@gmail.com>
2023-03-12 11:39:27 -0500

    append

---
+++ rust_near/target/debug/deps/rust_near-4874126bd7dc947a.d
@@ -0,0 +1,5 @@
+/home/simon/Escritorio/programación/rust/rust_near/target/debug/deps/rust_near-4874126bd7dc947a.rmeta: src/lib.rs
+
+/home/simon/Escritorio/programación/rust/rust_near/target/debug/deps/rust_near-4874126bd7dc947a.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust_near/target/debug/deps/rust_near-dea0cc4fb33a9c90.d
@@ -0,0 +1,5 @@
+/home/simon/Escritorio/programación/rust/rust_near/target/debug/deps/rust_near-dea0cc4fb33a9c90.rmeta: src/lib.rs
+
+/home/simon/Escritorio/programación/rust/rust_near/target/debug/deps/rust_near-dea0cc4fb33a9c90.d: src/lib.rs
+
+src/lib.rs:
